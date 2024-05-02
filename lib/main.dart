c84b3e56b31373c9896159f1c55a1aa6d6080f7d
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-05-03 02:17:25 +0300

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:store_app/core/app/env.variables.dart';
 import 'package:store_app/store_app.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const StoreApp());
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const StoreApp());
+  });
 }
--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/env.variables.dart';
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
@@ -6,6 +7,7 @@ class StoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
       title: 'Store App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
