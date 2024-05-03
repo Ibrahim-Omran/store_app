e08df19a97badb96db30c02881da0ccbdab83144
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-05-03 17:10:54 +0300

    connectivity plus done

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:store_app/core/app/env.variables.dart';
+import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
-
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
